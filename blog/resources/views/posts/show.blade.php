37ee0a63ace10b4230ef728b0996c206de5b2848
haykaz <haykazvoskanyan@gmail.com>
2022-05-03 00:07:22 +0400

    project push

---
+++ blog/resources/views/posts/show.blade.php
@@ -0,0 +1,53 @@
+@extends('posts.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Post</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+     
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                {{ $post->title }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $post->description }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Image:</strong>
+                <img src="/image/{{ $post->image }}" width="500px">
+            </div>
+        </div>
+        {{-- <form action="{{ route('comments.store') }}" method="POST" > --}}
+            @csrf
+            
+             <div class="row">
+                
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>description:</strong>
+                        <textarea class="form-control" style="height:150px" name="text" placeholder="text"></textarea>
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+             
+        {{-- </form> --}}
+    </div>
+@endsection
